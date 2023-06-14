0d8c2bfe4fdca7b628d6c1611dc8fc3d5347d92a
mahdi_nzari <mahdi.nazari.gb008@gmail.com>
2023-06-14 15:12:31 -0700

    feat: add metadata and robots.txt and sitemap.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,51 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "./globals.css";
 
-import localFont from '@next/font/local'
+import localFont from "@next/font/local";
 
-const myFont = localFont({
-  src:"/font/dana/woff2/Dana-Light.woff2" ,
-  variable : "--default-font",
-  weight:"400",
-  display:"swap"
-})
+/* ---------------- Dana Fonts ---------------- */
+const dana = localFont({
+  display: "swap",
+  variable: "--default-font",
+  src: [
+    {
+      path: "../public/font/dana/woff2/Dana-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+
+    {
+      path: "../public/font/dana/woff2/Dana-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/font/dana/woff2/Dana-DemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/font/dana/woff2/Dana-ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/font/dana/woff2/Dana-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../public/font/dana/woff2/Dana-Heavy.woff2",
+      weight: "990",
+      style: "normal",
+    },
+  ],
+});
 export const metadata = {
-  title: "وبسایت شخصی مهدی نظری",
+  title:{
+    default: "وبسایت مهدی نظری",
+    template:"%s | وبسایت مهدی نظری"
+  },
   description:
     "مهدی نظری هستم برنامه نویس فرانت اند که توی این حوضه یادمیگیره و آموزش هاش رو انتقال میده",
 };
@@ -23,7 +58,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className={`relative ${myFont.variable} font-dana`}>
+      <body className={`relative ${dana.variable} font-dana`}>
         <Navbar />
         {children}
         <Footer />
---
+++ app/shop/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "فروشگاه",
+};
+const page = () => {
+  return <div>فروشگاه</div>;
+};
+
+export default page;
